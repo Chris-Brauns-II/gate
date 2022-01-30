df55560d88c170d9a0ca3acf829019a9a0444021
Chris Brauns II <braunsie@gmail.com>
2022-01-30 12:38:56 -0500

    WIP

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "https://rubygems.org"
 
+gem 'cli-ui'
+gem 'tty'
+gem 'tty-box'
+
 gem "pry"
 gem "pry-byebug"
 gem "rspec"
--- lib/main.rb
+++ lib/main.rb
@@ -8,49 +8,11 @@ require "./lib/signal_log"
 require "./lib/time_log"
 require "./lib/wire"
 
-time = LogicTime.new
-
-event_bus = LogicBus.new
-
-a = Wire.new(name: "A")
-b = Wire.new(name: "B")
-
-c = Wire.new(name: "C")
-
-and_gate = AndGate.new(
-  a: a,
-  b: b,
-  event_bus: event_bus,
-  output_wire: c,
-  time: time 
-)
-
-d = Wire.new(name: "D")
-e = Wire.new(name: "E")
-
-second_and_gate = AndGate.new(
-  a: c,
-  b: d,
-  event_bus: event_bus,
-  output_wire: e,
-  time: time
-)
-
-event_bus
-  .push(LogicBusEvent.new(time: 0, value: false, wire: a))
-  .push(LogicBusEvent.new(time: 0, value: false, wire: b))
-  .push(LogicBusEvent.new(time: 0, value: true, wire: d))
-  .push(LogicBusEvent.new(time: 300, value: true, wire: a))
-  .push(LogicBusEvent.new(time: 600, value: true, wire: b))
-  .push(LogicBusEvent.new(time: 900, value: false, wire: b))
+require "cli/ui"
+require "tty-box"
 
 
-circuit = Circuit.new(event_bus: event_bus, logic_time: time)
+# box = TTY::Box.frame(width: 30, height: 10, title: {top_left: "TITLE", bottom_right: "v1.0"})
+box = TTY::Box.warn("Deploying application")
 
-time_log = TimeLog.new([a,b,c,d,e], time: time)
-
-time.run(finish: 1000)
-
-log = SignalLog.new(event_bus)
-
-puts log.log
+print box
